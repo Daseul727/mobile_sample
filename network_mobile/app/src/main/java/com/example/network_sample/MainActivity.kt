664aad1939d1f9f5b709e37d2c01741ea631310c
김다슬 <daseul727@gmail.com>
2024-03-29 10:34:07 +0900

    network sample - 데이터 바인딩

--- network_mobile/app/src/main/java/com/example/network_sample/MainActivity.kt
+++ network_mobile/app/src/main/java/com/example/network_sample/MainActivity.kt
@@ -2,7 +2,13 @@ package com.example.network_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.example.network_sample.databinding.ActivityMainBinding
 import com.example.network_sample.model.MainViewModel
+import com.example.network_sample.utils.NetworkResult
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -12,9 +18,52 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var mainViewModel: MainViewModel
+    private lateinit var _binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        fetchData()
+
+        _binding.imgRefresh.setOnClickListener {
+            fetchResponse()
+        }
+    }
+
+    private fun fetchData() {
+        fetchResponse()
+
+        mainViewModel.response.observe(this) { res->
+            when (res) {
+                is NetworkResult.Success -> {
+                    res.data?.let {
+                        _binding.imgDog.load(
+                            res.data.message
+                        ) {
+                            transformations(RoundedCornersTransformation(16f))
+                        }
+                    }
+                    _binding.pbDog.visibility = View.GONE
+                }
+                is NetworkResult.Error -> {
+                    _binding.pbDog.visibility = View.GONE
+                    Toast.makeText(
+                        this,
+                        res.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    _binding.pbDog.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun fetchResponse() {
+        mainViewModel.fetchDogResponse()
+        _binding.pbDog.visibility = View.VISIBLE
     }
 }
